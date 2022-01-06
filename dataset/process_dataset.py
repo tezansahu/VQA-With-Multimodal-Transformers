fff79431ed1f34a1d6ef159e390f209605167e12
Tezan Sahu <tezansahu@microsoft.com>
2022-01-06 19:08:20 +0530

    CSV files for train & eval; Answer space created

--- dataset/process_dataset.py
+++ dataset/process_dataset.py
@@ -1,17 +1,18 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import re
 import os
 
-image_pattern = re.compile("( (in |on |of )?(the |this )?(image\d*) )")
+image_pattern = re.compile("( (in |on |of )?(the |this )?(image\d*) \?)")
 
 with open("all_qa_pairs.txt") as f:
     qa_data = [x.replace("\n", "") for x in f.readlines()]
 
-with open("train_images_list.txt") as f:
-    train_imgs = [x.replace("\n", "") for x in f.readlines()]
+# with open("train_images_list.txt") as f:
+#     train_imgs = [x.replace("\n", "") for x in f.readlines()]
 
-with open("test_images_list.txt") as f:
-    test_imgs = [x.replace("\n", "") for x in f.readlines()]
+# with open("test_images_list.txt") as f:
+#     test_imgs = [x.replace("\n", "") for x in f.readlines()]
 
 df = pd.DataFrame({"question": [], "answer": [], "image_id":[], "image_path": []})
 
@@ -26,8 +27,21 @@ for i in range(0, len(qa_data), 2):
     }
     df = df.append(record, ignore_index=True)
 
-train_df = df[df.image_id.isin(train_imgs)]
-test_df = df[df.image_id.isin(test_imgs)]
+answer_space = []
+for ans in df.answer.to_list():
+    answer_space = answer_space + [ans] if "," not in ans else answer_space + ans.replace(" ", "").split(",") 
 
-train_df.to_csv("data_train.tsv", sep="\t", index=None)
-test_df.to_csv("data_eval.tsv", sep="\t", index=None)
+answer_space = list(set(answer_space))
+answer_space.sort()
+with open("answer_space.txt", "w") as f:
+    f.writelines("\n".join(answer_space))
+
+# train_df = df[df.image_id.isin(train_imgs)]
+# test_df = df[df.image_id.isin(test_imgs)]
+
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+train_df.to_csv("data_train.csv", index=None)
+test_df.to_csv("data_eval.csv", index=None)
+
+df.to_csv("data.csv", index=None)
